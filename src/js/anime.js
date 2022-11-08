4b12cd2eebbda6e476f531c204121e4d78727292
Marcelo Giati <mtgiati@gmail.com>
2022-11-08 14:17:05 -0300

    aut.01

--- src/js/anime.js
+++ src/js/anime.js
@@ -1,23 +1,23 @@
 const block = document.querySelectorAll('.block');
-const animation = anime.time({
+const animation = anime.timeline({
     targets: block,
-    easing: 'easseInOutExpo',
+    easing: 'easeInOutExpo',
     loop: true,
 }) 
 
-animation 
-.add({
-    scale: 0,
-    translateX: function(){ return anime.random (-360, 2100);},
-    translateY: function(){ return anime.random (-360, 2100);},
-    rotate: function(){ return anime.random (-360, 360);},
-    duration: function(){ return anime.randon(500, 3000);}
-})
+// animation 
+// .add({
+    // scale: 0,
+//     translateX: function(){ return anime.random (-360, 2100);},
+//     translateY: function(){ return anime.random (-360, 2100);},
+//     rotate: function(){ return anime.random (-360, 360);},
+//     duration: function(){ return anime.randon(500, 3000);}
+// })
 
-.add({
-    scale: 1,
-    translateX: 0,
-    translateY: 0,
-    rotate: 0,
-    duration: function(){ return anime.randon(500, 3000);}
-})
+// .add({
+//     scale: 1,
+//     translateX: 0,
+//     translateY: 0,
+//     rotate: 0,
+//     duration: function(){ return anime.randon(500, 3000);}
+// })
--- src/js/container.js
+++ src/js/container.js
@@ -4,3 +4,27 @@ for (var i = 0; i <= 400; i++) {
     block.classList.add('block');
     container.appendChild(block);
 }
+
+// const block = document.querySelectorAll('.block');
+// const animation = anime.timeline({
+//     targets: block,
+//     easing: 'easeInOutExpo',
+//     loop: true,
+// }) 
+
+// animation 
+// .add({
+    scale: 0,
+    //     translateX: function(){ return anime.random (-360, 2100);},
+    //     translateY: function(){ return anime.random (-360, 2100);},
+    //     rotate: function(){ return anime.random (-360, 360);},
+    //     duration: function(){ return anime.randon(500, 3000);}
+// })
+
+// .add({
+//     scale: 1,
+//     translateX: 0,
+//     translateY: 0,
+//     rotate: 0,
+//     duration: function(){ return anime.randon(500, 3000);}
+// })
